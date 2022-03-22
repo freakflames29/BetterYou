08ca059890f0a1e82f71c3561dc48b0a897ee4c4
Sourav Das <iamsouravdaswade420@gmail.com>
2022-03-22 17:52:03 +0530

    fixed explore users last task

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   # get '*path', to: 'welcome#index' #it will redirect all unknown routes to index in welcome
-  resources :user,except:[:new]
+  resources :user,except:[:new,:index]
+
+  get 'explore',to:'user#index'
 
   get 'signup',to:'user#new'
   post 'signup',to:'user#create'
