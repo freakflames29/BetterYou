a841759f36a8ef8efe83b0f85afd6e51c8255841
Sourav Das <iamsouravdaswade420@gmail.com>
2022-04-02 15:35:44 +0530

    change userpoints

--- db/migrate/20220320171023_create_user_points.rb
+++ db/migrate/20220320171023_create_user_points.rb
@@ -2,7 +2,7 @@ class CreateUserPoints < ActiveRecord::Migration[6.1]
   def change
     create_table :user_points do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :points,default:'0'
+      t.integer :points,default:0
 
       t.timestamps
     end
